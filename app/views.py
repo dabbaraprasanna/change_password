a63a1eec1bef545d9826b4a4f5fe398205ac18af
dabbaraprasanna <prasannalakshmidabbara23@gmail.com>
2024-06-05 16:34:35 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render
+
+# Create your views here.
+from app.forms import *
+from django.http import HttpResponse,HttpResponseRedirect
+from django.core.mail import send_mail
+
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+
+
+def registration(request):
+    EUFO=UserForm()
+    EPFO=ProfileForm()
+    d={'EUFO':EUFO,'EPFO':EPFO}
+    if request.method=='POST' and request.FILES:
+        NMUFDO=UserForm(request.POST)
+        NMPFDO=ProfileForm(request.POST,request.FILES)
+        if NMUFDO.is_valid() and NMPFDO.is_valid():
+            MUFDO=NMUFDO.save(commit=False)
+            pw=NMUFDO.cleaned_data['password']
+            MUFDO.set_password(pw)
+            MUFDO.save()
+            send_mail('registration',
+            'registration is successful',
+            'prasannalakshmidabbara23@gmail.com',
+            [MUFDO.email],
+            fail_silently=False,)            
+            MPFDO=NMPFDO.save(commit=False)
+            MPFDO.username=MUFDO
+            MPFDO.save()
+            return HttpResponse('Registration is Successfull')
+        else:
+            return HttpResponse('Invalid data')
+    return render(request,'registration.html',d)
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('Invalid Credentials')
+    return render(request,'user_login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+
+@login_required
+def profile_display(request):
+    username=request.session.get('username')
+    UO=User.objects.get(username=username)
+    PO=Profile.objects.get(username=UO)
+    d={'UO':UO,'PO':PO}
+    return render(request,'profile_display.html',d)
+
+@login_required
+def change_password(request):
+    if request.method=='POST':
+        username=request.session.get('username')
+        UO=User.objects.get(username=username)
+        nw=request.POST['nw']
+        UO.set_password(nw)
+        UO.save()
+        return HttpResponse('password is changed successfuly')
+
+    return render(request,'change_password.html')
